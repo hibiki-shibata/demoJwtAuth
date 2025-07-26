23eadeb508976d56b629dfdc3819aefa258a3226
hibiki-shibata <hibikishibata1853@gmail.com>
2025-07-27 01:56:30 +0900

    exception Middleware - can handle any exceptions now

--- middleware/exceptionMiddleware.ts
+++ middleware/exceptionMiddleware.ts
@@ -2,14 +2,25 @@ import { Request, Response } from 'express';
 import httpException from '../exceptions/httpException';
 
 // err: httpException is custome error class passed from controller in next(new customException('message'))
-export const exceptionMiddleware = (err: httpException, req: Request, res: Response, next: Function) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    
+export const exceptionMiddleware = (err: Error, req: Request, res: Response, next: Function) => {
+    let statusCode: number;
+    let message: string;
+
+    if (!(err instanceof httpException)) {
+        statusCode = 500;
+        message = 'Internal Server Error';
+    } else {
+        statusCode = err.statusCode || 500;
+        message = err.message || 'An error occurred';
+    }
+
+    // const statusCode = isHttpException.statusCode ? err.statusCode : 500;
+    // const message = isHttpException.message || 'Internal Server Error';
+
     res.status(statusCode).json({
         status: 'error',
         statusCode,
         message,
     });
-    
+
 }
